7c8505befe22be1d5cd3ebda5a9fa96939db988c
Sebastian <sebastian.cj456@gmail.com>
2026-04-26 22:05:40 -0500

    corrijo main

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,38 @@
 public class Calculadora {
 
-    public int sumar(int a, int b){
-        return a + b;
+    private String historial;
+    private static int totalOperaciones = 0;
+
+    public Calculadora() {
+        this.historial = "";
+    }
+
+    public int sumar(int a, int b) {
+        int resultado = a + b;
+        totalOperaciones++;
+        historial += a + "+" + b + "=" + resultado + "\n";
+        return resultado;
+    }
+
+    public double sumar(double a, double b) {
+        double resultado = a + b;
+        totalOperaciones++;
+        historial += a + "+" + b + "=" + resultado + "\n";
+        return resultado;
+    }
+
+    public int sumar(int a, int b, int c) {
+        int resultado = a + b + c;
+        totalOperaciones++;
+        historial += a + "+" + b + "+" + c + "=" + resultado + "\n";
+        return resultado;
+    }
+
+    public static int getTotalOperaciones() {
+        return totalOperaciones;
+    }
+
+    public void mostrarHistorial() {
+        System.out.println(historial);
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main {
+
     public static void main(String[] args) {
-        Estudiante e = new Estudiante("Sebastian", 18);
-        e.mostrar();
+
+        Estudiante e1 = new Estudiante("Sebastian", 18);
+        e1.mostrar();
+
+        Calculadora calc = new Calculadora();
+
+        System.out.println("Suma 1: " + calc.sumar(5, 3));
+        System.out.println("Suma 2: " + calc.sumar(2.5, 3.5));
+        System.out.println("Suma 3: " + calc.sumar(1, 2, 3));
+
+        calc.mostrarHistorial();
+
+        System.out.println("Total operaciones: " + Calculadora.getTotalOperaciones());
     }
 }
