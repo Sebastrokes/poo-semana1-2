189f8e9f1f3ba71388cbbf517717af084f295a0d
Sebastian <sebastian.cj456@gmail.com>
2026-04-26 20:32:42 -0500

    fix: corrijo variable notaFinal

--- Estudiante.java
+++ Estudiante.java
@@ -1,13 +1,26 @@
 public class Estudiante {
-    private String nombre;
-    private int nota;
 
-    public Estudiante(String nombre, int nota){
+    private String nombre;
+    private int notaFinal;
+
+    public Estudiante(String nombre, int notaFinal){
         this.nombre = nombre;
-        this.nota = nota;
+        this.notaFinal = notaFinal;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getNotaFinal(){
+        return notaFinal;
+    }
+
+    public void setNotaFinal(int notaFinal){
+        this.notaFinal = notaFinal;
     }
 
     public void mostrar(){
-        System.out.println(nombre + " - " + nota);
+        System.out.println(nombre + " - " + notaFinal);
     }
 }
